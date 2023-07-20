a1e63eca2d51e77b98393e8be498b6dc7e6a9140
noname <nomail>
2023-07-20 11:00:09 +0900

    Initial Commit

---
+++ HiddenTimer/HiddenTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiddenTimerApp.swift
+//  HiddenTimer
+//
+//  Created by Mincle on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct HiddenTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
